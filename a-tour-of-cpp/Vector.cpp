5f697d86643cf1d59f1a947cb91f4f954dc5fa91
Stan Slupecki <sslup@hermetikos.com>
2019-05-24 15:25:46 -0400

    Restructures code to use Vector class

---
+++ a-tour-of-cpp/Vector.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+// a class is a basically handle that holds a pointer to the attributes and methods
+class Vector {
+public:
+	// constructor w/ shorthand initialization
+	// this is called a member initialization list
+	Vector(int s) :elem(new double[s]), sz(s) {}
+	// overwrite the element access operator
+	//	so it uses the standard array interface for access
+	double& operator[](int i) { return elem[i]; }
+	int size() { return sz; }
+private:
+	double* elem; // pointer to elements
+	int sz;	// size of the vector
+};
+
+double read_and_sum(int s) {
+	// read s ints from cin and return their sum
+	Vector v(s); // init a Vector of size s
+	for (int i = 0; i != v.size(); ++i)
+		cin >> v[i];
+
+	double sum = 0;
+	for (int i = 0; i != s; ++i)
+		sum += v[i];
+
+	return sum;
+}
+
+/* VECTOR AS A STRUCTURE
+// Vector struct that will be built up to demonstrate C++
+struct Vector
+{
+	int sz;	// num of elements
+	double* elem; // pointer to elements
+};
+
+void vector_init(Vector& v, int s) {
+	v.elem = new double[s]; // allocate array of s doubles from heap
+	v.sz = s;
+}
+
+double read_and_sum(int s) {
+	// read s ints from cin and return their sum
+	Vector v;
+	vector_init(v, s); // allocate s elements in v
+	for (int i = 0; i != s; ++i)
+		cin >> v.elem[i];
+
+	double sum = 0;
+	for (int i = 0; i != s; ++i)
+		sum += v.elem[i];
+
+	return sum;
+}
+*/
--- a-tour-of-cpp/main.cpp
+++ a-tour-of-cpp/main.cpp
@@ -1,29 +1,13 @@
 #include <iostream>
+#include "Vector.cpp"
 
 using namespace std;
 
-// Vector struct that will be built up to demonstrate C++
-struct Vector
-{
-	int sz;	// num of elements
-	double* elem; // pointer to elements
-};
-
-void vector_init(Vector& v, int s) {
-	v.elem = new double[s]; // allocate array of s doubles from heap
-	v.sz = s;
-}
-
-double read_and_sum(int s) {
-	// read s ints from cin and return their sum
-	Vector v;
-	vector_init(v, s); // allocate s elements in v
-	for (int i = 0; i != s; ++i)
-		cin >> v.elem[i];
-
-	double sum = 0;
-	for (int i = 0; i != s; ++i)
-		sum += v.elem[i];
-
-	return sum;
+int main() {
+	Vector v(6);
+	cout << v[0] << endl;
+	
+	char wait;
+	cin >> wait;
+	return 0;
 }
