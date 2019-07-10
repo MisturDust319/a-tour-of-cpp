30c01cd60dde085379c56b03f957ffaeccf6d955
Stan Slupecki <sslup@hermetikos.com>
2019-07-09 20:06:32 -0400

    finishes the namespaces example

--- a-tour-of-cpp/Namespaces.cpp
+++ a-tour-of-cpp/Namespaces.cpp
@@ -5,29 +5,34 @@
 
 namespace My_Code {
 	class complex {
+	public:
 		complex(int x1, int x2)
 			: x1{ x1 }, x2{ x2 }
 		{}
 
-		float real() {
-			return (float)x1;
+		int real() {
+			return x1;
 		}
-
+	private:
 		int x1, x2;
 	};
 
 	complex sqrt(complex complex) {
-		return ;
+		return complex;
 	}
 	// insert more here
 
 	int main();
+
+	int My_Code::main() {
+		complex z{ 1, 2 };
+		auto z2 = sqrt(z);
+
+		std::cout << '{' << z2.real();
+		// and so on
+	}
 }
 
-int My_Code::main() {
-	complex z{ 1, 2 };
-	auto z2 = sqrt(z);
-
-	std::cout << '{' << z2.real();
-	// and so on
-}
+// make the default namespace by qualifying it with 
+//	'using namespace'
+using namespace My_Code;
